a15504e3b4365d9681d0c2feaecf3f3fe14530dc
Your Name <you@example.com>
2025-01-13 20:26:18 +0100

    norm check

--- src/io_count_line.c
+++ src/io_count_line.c
@@ -6,7 +6,7 @@
 /*   By: antoinemura <antoinemura@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 05:09:41 by antoinemura       #+#    #+#             */
-/*   Updated: 2025/01/07 16:17:29 by antoinemura      ###   ########.fr       */
+/*   Updated: 2025/01/13 20:26:01 by antoinemura      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ int	io_count_line(int fd)
 {
 	char	buffer[1024];
 	int		bytes_read;
-	int		line_count = 0;
+	int		line_count;
 	int		i;
 
+	line_count = 0;
 	bytes_read = read(fd, buffer, sizeof(buffer));
 	while (bytes_read > 0)
 	{
